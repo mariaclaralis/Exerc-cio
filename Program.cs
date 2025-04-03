13c1c412eebc9b21598595a7556bee81302ac4df
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-03 11:26:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Solicita ao usuário um número inteiro
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Ver se o número é par ou ímpar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é PAR.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ÍMPAR.");
+        }
+
+
+    }
+}
